90d9dd8ab2c6a708b89fe52e672a8c726a2f90f8
haileypressnell <hailey.pressnell@yahoo.com>
2018-07-12 17:00:47 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -25,7 +25,17 @@ end
 
 #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
 def amount_of_chocolate_chips(perfect_10_recipe)
-  
+ perfect_10_recipe = {
+   :almond_flour => "2 cups",
+   :gluten_free_whole_oats => "1 cup", 
+   :kosher_salt => "1/2 tea spoon",
+   :baking_powder => "1/2 tea spoon",
+   :baking_soda => "1/4 tea spoon",
+   :Xanthan_Gum => "1/2 tea spoon",
+   :slivered_almonds => "1/4 cup",
+   :mini_dark_chocolate_chips => "3/4 cup",
+   :olive_oil => "1/2 cup",
+   :agave => "1/4 cup"
 end
 
 #3: Use `puts` to print out every ingredient and amount in the hash by iterating through the collection. You'll want to use the `each` method.
