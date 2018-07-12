f7651a1477af75508949b550988ad1c05013801e
haileypressnell <hailey.pressnell@yahoo.com>
2018-07-12 17:18:03 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -30,10 +30,9 @@ puts " "
 def ingredients_and_amounts(perfect_10_recipe)
   perfect_10_recipe.each do |ingredient, amount|
     puts "INGREDIENT: #{ingredient}"
-    puts "AMOUNT: #{amount}""
+    puts "AMOUNT: #{amount}"
 end
-puts "INGREDIENTS & AMOUNTS:"
-puts ingredients_and_amounts(perfect_10_recipe)
+
 
 
 
